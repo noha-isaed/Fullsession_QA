f6ab2f33a4c98fba60fd93b332b154386a78a7d1
noha_isaed <thahernoha@gmail.com>
2021-10-05 23:08:00 +0300

    session playlist card

--- cypress/integration/Segments/sessions.js
+++ cypress/integration/Segments/sessions.js
@@ -13,17 +13,35 @@ describe('Session Playlist' , () => {
         cy.get('[formcontrolname="password"]').focus().type('test@FS21');
         cy.wait(1000);
         cy.get('button.form-control').should('be.visible').click();
-        cy.wait(30000) ;
+        cy.wait(7000) ;
 
         cy.url().should ('eq' , "https://test.fullsession.io/insights");
         cy.get('users-card > .user-sessions > .header > .right > .refresh > fs-clock > svg').should('be.visible').click();
 
         cy.get('users-card > .user-sessions > .header > .right > .zoom-in > fs-full-size > .clickable').should('be.visible').click();
-        cy.wait(2000);
+        cy.wait(4000);
         cy.url().should ('eq' , "https://test.fullsession.io/insights/playlist");
 
         cy.get('.go-back ').click();
-        cy.url().should ('eq' , "https://test.fullsession.io/insights");
+        cy.url().should ('eq' , "https://test.fullsession.io/insights");  
+        
+        cy.get(":nth-child(1) > .item-left > .two-line ").find('.visitor-id').invoke('text').then((text) => {
+
+            cy.get('.btn').should('be.visible').click();
+            cy.get('[ng-reflect-filter-name="userId"] > .container > .text').should('be.visible').click();
+            cy.get('.group-input > .fs-form-control').focus().type(text.trim());    
+            cy.get('.input-container').click();      
+        });
+        
+        cy.get('.container > .meta').should('include.text' , '1-1 of 1 People');
+        cy.get('fs-play-border > svg').should('be.visible').click();
+        cy.wait(2000)
+       
+        cy.get('.back').should('be.visible').click();
+        cy.url().should ('eq' , "https://test.fullsession.io/insights");  
+
+
+
 
 
 })
