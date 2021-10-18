019ab0fd8024332fe87cf5e43db7c85318e02ef0
noha_isaed <thahernoha@gmail.com>
2021-10-18 10:14:58 +0300

    edit on test error clicks csrd

--- cypress/integration/Segments/error_clicks.js
+++ cypress/integration/Segments/error_clicks.js
@@ -156,7 +156,7 @@ describe('Error Click Card' , () => {
 
   it('Ckeck Click On URL' , ()=> {
 
-    const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im50VzVuOFNXdGk4SFBIWXU3eklnMyJ9.eyJuaWNrbmFtZSI6ImhhbWVkYWJkZWxoYXEiLCJuYW1lIjoiaGFtZWRhYmRlbGhhcUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMjU0YmM2Nzg5MjRmZjc5MDIwOGRhMGVhMTUxZjY2YjU_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZoYS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMS0xMC0xN1QxNjozODowOC44MDJaIiwiZW1haWwiOiJoYW1lZGFiZGVsaGFxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2Rldi1mdWxsc2Vzc2lvbi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjBmZDEyZjdkMDU2NzUwMDcwZDk1OWU3IiwiYXVkIjoiczVzeDFONnhlOG9sZGVmb1JSV09CV1FEeno0UEdkODMiLCJpYXQiOjE2MzQ0ODg2ODgsImV4cCI6MTYzNDUyNDY4OH0.eSThW5v8fYvvVME5ziCNPf2osZYUBJHSyLuwX7AtB0rvHdQbuNRgKModxT4HGV4LykiVI-41aL6lK-bsE4uv_fxOM4xYbigQTRhLGdoDlyrxiQOLA8EPLjgKfY2UOCQPH7lIT-NdG4QUwBk2Vzq06LVeu8jH66W4m9Xj14BlXzN6_EaEYEYPzt7lprSTrqL_vt7_3xSwW8_NPnw5vonJILLe8-h_U1I6lo9M_f5MHfMl3TF-C5dfdlnN-vS-n-iaFsmBQGwaZeLlxMLe_ohbLlIR-pM8cLWsqGHSiPvX8-NM7SLjiK920_2hOQDq4ctf1rkrhFZMNMtYbcLZ1uV9Ng"
+    const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im50VzVuOFNXdGk4SFBIWXU3eklnMyJ9.eyJuaWNrbmFtZSI6ImhhbWVkYWJkZWxoYXEiLCJuYW1lIjoiaGFtZWRhYmRlbGhhcUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMjU0YmM2Nzg5MjRmZjc5MDIwOGRhMGVhMTUxZjY2YjU_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZoYS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMS0xMC0xOFQwNjozODoyOC4xMTNaIiwiZW1haWwiOiJoYW1lZGFiZGVsaGFxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2Rldi1mdWxsc2Vzc2lvbi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjBmZDEyZjdkMDU2NzUwMDcwZDk1OWU3IiwiYXVkIjoiczVzeDFONnhlOG9sZGVmb1JSV09CV1FEeno0UEdkODMiLCJpYXQiOjE2MzQ1MzkxMDgsImV4cCI6MTYzNDU3NTEwOH0.ZZnoPSQ488WyTOsF1nS4hdic6al4XpVCErG89e6xetuNqTzF_aLVJ1oB9DTAjn-Rg5STkJOnDxQoPzqxKGkAbs1j5X0wcUxiilwr3z4ypRgB3tZ1JR4MLV2itRJ5SV35EmaWu_Gncl__qvE8FilO1cFO1cMtcVvYYSvYmHsczCoXagAQ25IeoLrN54woup8xnGZC3t3rcF6WX0bcUHoPMuhovb9vLAw0bGcI-nY6QPpVbLY8LDlJLsQjlIDa9tvPZMkieyJCeBNnxs3bvfHzRh8bLMDtrhxf-o7UbzKJgO9YVqyX6cBrt3SQJ6sFV4sNK0u9emOjKTRLGBYkdzUt6Q'
     const authorization = `Bearer ${ token }`;
 
     cy.visit('https://test.fullsession.io/login');
@@ -195,8 +195,8 @@ describe('Error Click Card' , () => {
             "JSONquery": {
                 "startTime": [
                      {
-                      "from": 1633895182715,
-                       "to": 1634499982734
+                      "from": 1633934389813,
+                       "to": 1634539189856
                      }
                     ],
                 "visited" :[
@@ -218,21 +218,20 @@ describe('Error Click Card' , () => {
                   expect(response.body).to.have.property('data')
                   const array = response.body.data.sessions ; 
 
-                  cy.get(".group-input > .fs-form-control").invoke('text').then((text) => {
-                    cy.log(text.trim())
-                    // for(var i =0 ; i< array[0].visitedPgs.length ; i++){
-                      
-                    //   if((array[0].visitedPgs[i].page).localeCompare( text.trim())){
-                    //     expect(array[0].visitedPgs[i].page).to.equal(text.trim())
-                    //   }
-                    // }
+                  cy.get(".input-container > .group-input > .fs-form-control").invoke('val').then(sometext =>{
+                    for(var i =0 ; i< array[0].visitedPgs.length ; i++){
+                      if((array[0].visitedPgs[i].page) === ( sometext)){
+                        cy.log(array[0].visitedPgs[i].page)
+                        expect(array[0].visitedPgs[i].page).to.equal(sometext)
+                      }
+                    }
 
-                    // for(var i =0 ; i< array[1].visitedPgs.length ; i++){
-                    //   cy.log("33")
-                    //   if((array[1].visitedPgs[i].page).localeCompare( text.trim())){
-                    //     expect(array[1].visitedPgs[i].page).to.equal(text.trim())
-                    //   }
-                    // }
+                    for(var i =0 ; i< array[1].visitedPgs.length ; i++){
+                      if((array[1].visitedPgs[i].page) === (sometext)){
+                        cy.log(array[1].visitedPgs[i].page)
+                        expect(array[1].visitedPgs[i].page).to.equal(sometext)
+                      }
+                    }
                   })
                 
                   
