43ebe92656418ccf887c2b1af901050527773dd2
noha_isaed <thahernoha@gmail.com>
2021-10-18 13:36:53 +0300

    test rage clicks card

--- cypress/integration/Segments/rage_clicks.js
+++ cypress/integration/Segments/rage_clicks.js
@@ -1,11 +1,11 @@
 /// <reference types="cypress" />
 
-describe('Error Click Card' , () => {
+describe('Rage Click Card' , () => {
 
     it('Ckeck Refresh Icon' , ()=> {
 
-            const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im50VzVuOFNXdGk4SFBIWXU3eklnMyJ9.eyJuaWNrbmFtZSI6ImhhbWVkYWJkZWxoYXEiLCJuYW1lIjoiaGFtZWRhYmRlbGhhcUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMjU0YmM2Nzg5MjRmZjc5MDIwOGRhMGVhMTUxZjY2YjU_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZoYS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMS0xMC0xN1QxNjozODowOC44MDJaIiwiZW1haWwiOiJoYW1lZGFiZGVsaGFxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2Rldi1mdWxsc2Vzc2lvbi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjBmZDEyZjdkMDU2NzUwMDcwZDk1OWU3IiwiYXVkIjoiczVzeDFONnhlOG9sZGVmb1JSV09CV1FEeno0UEdkODMiLCJpYXQiOjE2MzQ0ODg2ODgsImV4cCI6MTYzNDUyNDY4OH0.eSThW5v8fYvvVME5ziCNPf2osZYUBJHSyLuwX7AtB0rvHdQbuNRgKModxT4HGV4LykiVI-41aL6lK-bsE4uv_fxOM4xYbigQTRhLGdoDlyrxiQOLA8EPLjgKfY2UOCQPH7lIT-NdG4QUwBk2Vzq06LVeu8jH66W4m9Xj14BlXzN6_EaEYEYPzt7lprSTrqL_vt7_3xSwW8_NPnw5vonJILLe8-h_U1I6lo9M_f5MHfMl3TF-C5dfdlnN-vS-n-iaFsmBQGwaZeLlxMLe_ohbLlIR-pM8cLWsqGHSiPvX8-NM7SLjiK920_2hOQDq4ctf1rkrhFZMNMtYbcLZ1uV9Ng'
-            const authorization = `Bearer ${ token }`;
+            const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im50VzVuOFNXdGk4SFBIWXU3eklnMyJ9.eyJuaWNrbmFtZSI6ImhhbWVkYWJkZWxoYXEiLCJuYW1lIjoiaGFtZWRhYmRlbGhhcUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMjU0YmM2Nzg5MjRmZjc5MDIwOGRhMGVhMTUxZjY2YjU_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZoYS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMS0xMC0xOFQwNjozODoyOC4xMTNaIiwiZW1haWwiOiJoYW1lZGFiZGVsaGFxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2Rldi1mdWxsc2Vzc2lvbi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjBmZDEyZjdkMDU2NzUwMDcwZDk1OWU3IiwiYXVkIjoiczVzeDFONnhlOG9sZGVmb1JSV09CV1FEeno0UEdkODMiLCJpYXQiOjE2MzQ1MzkxMDgsImV4cCI6MTYzNDU3NTEwOH0.ZZnoPSQ488WyTOsF1nS4hdic6al4XpVCErG89e6xetuNqTzF_aLVJ1oB9DTAjn-Rg5STkJOnDxQoPzqxKGkAbs1j5X0wcUxiilwr3z4ypRgB3tZ1JR4MLV2itRJ5SV35EmaWu_Gncl__qvE8FilO1cFO1cMtcVvYYSvYmHsczCoXagAQ25IeoLrN54woup8xnGZC3t3rcF6WX0bcUHoPMuhovb9vLAw0bGcI-nY6QPpVbLY8LDlJLsQjlIDa9tvPZMkieyJCeBNnxs3bvfHzRh8bLMDtrhxf-o7UbzKJgO9YVqyX6cBrt3SQJ6sFV4sNK0u9emOjKTRLGBYkdzUt6Q '
+             const authorization = `Bearer ${ token }`;
 
             cy.visit('https://test.fullsession.io/login');
             cy.wait(2000);
@@ -26,15 +26,15 @@ describe('Error Click Card' , () => {
               body: {
                   "customerID": "krivsgmdqk",
                   "siteID": "krivvr1rtzc",
-                   "field": "noErrorEvents" ,
+                   field: "noFrustrationClicks",
                    "searchAfterID": 0,
                    "size": 2 ,
                    "userId": "auth0|60fd12f7d056750070d959e7" ,
                   "JSONquery": {
                     "startTime": [
                       {
-                        "from": 1633883980122,
-                        "to": 1634488780175
+                        "from": 1633945024376,
+                        "to": 1634549824427
                       }
                     ]
                   }
@@ -48,32 +48,36 @@ describe('Error Click Card' , () => {
                       expect(response.body).has.property('message', 'success');
                       expect(response.body).to.have.property('data')
                       const array = response.body.data ; 
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div").invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div").invoke('text').then((text) => {
                         expect(text.replace(/\s/g, '')).to.equal(array.buckets[0].key)
                       })
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(2) > .item-data > .meta-key > div").invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(2) > .item-data > .meta-key > div").invoke('text').then((text) => {
                         expect(text.replace(/\s/g, '')).to.equal(array.buckets[1].key)                      
                       })
 
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .value").invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .value").invoke('text').then((text) => {
                         expect(text == array.buckets[0].sumClicks.value)
                       })
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(2) > .item-data > .meta-value > .value").invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(2) > .item-data > .meta-value > .value").invoke('text').then((text) => {
                         expect(text == array.buckets[1].sumClicks.value)                      
                       })
-                         cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
-                           var num = (array.buckets[0].sumClicks.value/array.count)*100; 
-                           expect(text.replace(/\s/g, '')).to.equal(Math.round (num)+ "%")
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
+                          var num = (array.buckets[0].sumClicks.value/array.count)*100; 
+                          cy.log(num)
+                          cy.log(text)
+                          expect(text.replace(/\s/g, '')).to.equal(Math.round (num)+ "%")
                       })
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
-                            var num = (array.buckets[5].sumClicks.value/array.count)*100; 
-                            expect(text.replace(/\s/g, '')).to.equal(Math.round (num)+ "%")                     
-                      })
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
+                          var num = (array.buckets[5].sumClicks.value/array.count)*100; 
+                          cy.log(num)
+                          cy.log(text)
+                          expect(text.replace(/\s/g, '')).to.equal(Math.round (num)+ "%")                     
+                    })
                     
           })
 
 
-          cy.get('error-clicks > .user-sessions > :nth-child(1) > .right > .refresh > fs-clock ').should('be.visible').click();
+          cy.get('dead-click > .user-sessions > :nth-child(1) > .right > .refresh > fs-clock > svg').click();
           cy.wait(2000)
     })
 
@@ -81,7 +85,7 @@ describe('Error Click Card' , () => {
     it('Ckeck Full Width Icon' , ()=> {
 
 
-      const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im50VzVuOFNXdGk4SFBIWXU3eklnMyJ9.eyJuaWNrbmFtZSI6ImhhbWVkYWJkZWxoYXEiLCJuYW1lIjoiaGFtZWRhYmRlbGhhcUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMjU0YmM2Nzg5MjRmZjc5MDIwOGRhMGVhMTUxZjY2YjU_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZoYS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMS0xMC0xN1QxNjozODowOC44MDJaIiwiZW1haWwiOiJoYW1lZGFiZGVsaGFxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2Rldi1mdWxsc2Vzc2lvbi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjBmZDEyZjdkMDU2NzUwMDcwZDk1OWU3IiwiYXVkIjoiczVzeDFONnhlOG9sZGVmb1JSV09CV1FEeno0UEdkODMiLCJpYXQiOjE2MzQ0ODg2ODgsImV4cCI6MTYzNDUyNDY4OH0.eSThW5v8fYvvVME5ziCNPf2osZYUBJHSyLuwX7AtB0rvHdQbuNRgKModxT4HGV4LykiVI-41aL6lK-bsE4uv_fxOM4xYbigQTRhLGdoDlyrxiQOLA8EPLjgKfY2UOCQPH7lIT-NdG4QUwBk2Vzq06LVeu8jH66W4m9Xj14BlXzN6_EaEYEYPzt7lprSTrqL_vt7_3xSwW8_NPnw5vonJILLe8-h_U1I6lo9M_f5MHfMl3TF-C5dfdlnN-vS-n-iaFsmBQGwaZeLlxMLe_ohbLlIR-pM8cLWsqGHSiPvX8-NM7SLjiK920_2hOQDq4ctf1rkrhFZMNMtYbcLZ1uV9Ng'
+      const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Im50VzVuOFNXdGk4SFBIWXU3eklnMyJ9.eyJuaWNrbmFtZSI6ImhhbWVkYWJkZWxoYXEiLCJuYW1lIjoiaGFtZWRhYmRlbGhhcUBnbWFpbC5jb20iLCJwaWN0dXJlIjoiaHR0cHM6Ly9zLmdyYXZhdGFyLmNvbS9hdmF0YXIvMjU0YmM2Nzg5MjRmZjc5MDIwOGRhMGVhMTUxZjY2YjU_cz00ODAmcj1wZyZkPWh0dHBzJTNBJTJGJTJGY2RuLmF1dGgwLmNvbSUyRmF2YXRhcnMlMkZoYS5wbmciLCJ1cGRhdGVkX2F0IjoiMjAyMS0xMC0xOFQwNjozODoyOC4xMTNaIiwiZW1haWwiOiJoYW1lZGFiZGVsaGFxQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpc3MiOiJodHRwczovL2Rldi1mdWxsc2Vzc2lvbi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjBmZDEyZjdkMDU2NzUwMDcwZDk1OWU3IiwiYXVkIjoiczVzeDFONnhlOG9sZGVmb1JSV09CV1FEeno0UEdkODMiLCJpYXQiOjE2MzQ1MzkxMDgsImV4cCI6MTYzNDU3NTEwOH0.ZZnoPSQ488WyTOsF1nS4hdic6al4XpVCErG89e6xetuNqTzF_aLVJ1oB9DTAjn-Rg5STkJOnDxQoPzqxKGkAbs1j5X0wcUxiilwr3z4ypRgB3tZ1JR4MLV2itRJ5SV35EmaWu_Gncl__qvE8FilO1cFO1cMtcVvYYSvYmHsczCoXagAQ25IeoLrN54woup8xnGZC3t3rcF6WX0bcUHoPMuhovb9vLAw0bGcI-nY6QPpVbLY8LDlJLsQjlIDa9tvPZMkieyJCeBNnxs3bvfHzRh8bLMDtrhxf-o7UbzKJgO9YVqyX6cBrt3SQJ6sFV4sNK0u9emOjKTRLGBYkdzUt6Q'
       const authorization = `Bearer ${ token }`;
 
       cy.visit('https://test.fullsession.io/login');
@@ -96,10 +100,10 @@ describe('Error Click Card' , () => {
 
       cy.url().should ('eq' , "https://test.fullsession.io/insights");
 
-      cy.get('error-clicks > .user-sessions > :nth-child(1) > .right > .zoom-in > fs-full-size > .clickable').click();
+      cy.get('dead-click > .user-sessions > :nth-child(1) > .right > .zoom-in > fs-full-size > .clickable').click();
       cy.wait(4000);
       cy.get('#cdk-overlay-0 > .ng-trigger').should('have.css' , 'color' , 'rgba(0, 0, 0, 0.85)')
-      cy.get('error-clicks > .user-sessions').should('be.visible')
+      cy.get('dead-click > .user-sessions').should('be.visible')
       cy.wait(2000)
 
               cy.request({
@@ -108,15 +112,15 @@ describe('Error Click Card' , () => {
               body: {
                   "customerID": "krivsgmdqk",
                   "siteID": "krivvr1rtzc",
-                   "field": "noErrorEvents" ,
+                   "field": "noFrustrationClicks" ,
                    "searchAfterID": 0,
                    "size": 6 ,
                    "userId": "auth0|60fd12f7d056750070d959e7" ,
                   "JSONquery": {
                     "startTime": [
                       {
-                        "from": 1633892692075,
-                        "to": 1634497492104
+                        "from": 1633947278161,
+                        "to": 1634552078305
                       }
                     ]
                   }
@@ -130,34 +134,34 @@ describe('Error Click Card' , () => {
                       expect(response.body).has.property('message', 'success');
                       expect(response.body).to.have.property('data')
                       const array = response.body.data ; 
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div").last().invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div").last().invoke('text').then((text) => {
                         expect(text.replace(/\s/g, '')).to.equal(array.buckets[0].key)
                       })
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-key > div").last().invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-key > div").last().invoke('text').then((text) => {
                         expect(text.replace(/\s/g, '')).to.equal(array.buckets[5].key)                      
                       })
 
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .value").last().invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .value").last().invoke('text').then((text) => {
                         expect(text == array.buckets[0].sumClicks.value)
                       })
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-value > .value").last().invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-value > .value").last().invoke('text').then((text) => {
                         expect(text == array.buckets[5].sumClicks.value)                      
                       })
                     
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
                         var num = (array.buckets[0].sumClicks.value/array.count)*100; 
-                        expect(text.replace(/\s/g, '')).to.equal(Math.round (num)+ "%")
+                        expect(text.replace(/\s/g, '')).to.equal(Math.ceil (num)+ "%")
                       })
-                      cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
+                      cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(6) > .item-data > .meta-value > .percentage").last().invoke('text').then((text) => {
                         var num = (array.buckets[5].sumClicks.value/array.count)*100; 
-                        expect(text.replace(/\s/g, '')).to.equal(Math.round (num)+ "%")                     
+                        expect(text.replace(/\s/g, '')).to.equal(Math.ceil (num)+ "%")                     
                       })
           })
 
-      cy.get('error-clicks > .user-sessions > .header > .right > .refresh > fs-clock > svg').last().should('be.visible').click();
+      cy.get('dead-click > .user-sessions > .header > .right > .refresh > fs-clock > svg').last().should('be.visible').click();
       
-      cy.get(' error-clicks > .user-sessions > :nth-child(1) > .right > .zoom-in > fs-full-size > .clickable').last().should('be.visible').click();
-      cy.get('error-clicks > .user-sessions').last().not().should('be.visible')
+      cy.get('dead-click> .user-sessions > :nth-child(1) > .right > .zoom-in > fs-full-size > .clickable').last().should('be.visible').click();
+      cy.get('dead-click > .user-sessions').last().not().should('be.visible')
 
   })
 
@@ -180,8 +184,8 @@ describe('Error Click Card' , () => {
     cy.url().should ('eq' , "https://test.fullsession.io/insights"); 
 
     
-    cy.get("error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div").invoke('text').then((text) => {
-        cy.get('error-clicks > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div').should('be.visible').click();
+    cy.get("dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div").invoke('text').then((text) => {
+        cy.get('dead-click > .user-sessions > :nth-child(3) > fs-histogram.ng-star-inserted > .container > .content > :nth-child(1) > .item-data > .meta-key > div').click();
         cy.wait(9000)
         cy.url().should ('eq' , "https://test.fullsession.io/insights/playlist");  
         // cy.get('.modal').should('be.visible')
@@ -194,7 +198,7 @@ describe('Error Click Card' , () => {
           body: {
             "customerID": "krivsgmdqk",
             "siteID": "krivvr1rtzc",
-            "field": "noErrorEvents" ,
+            "field": "noFrustrationClicks" ,
             "searchAfter": null,
             "sortBy": null,
             "sortOrder": null,
@@ -203,8 +207,8 @@ describe('Error Click Card' , () => {
             "JSONquery": {
                 "startTime": [
                      {
-                      "from": 1633934389813,
-                       "to": 1634539189856
+                      "from": 1633948428350,
+                       "to": 1634553228414
                      }
                     ],
                 "visited" :[
